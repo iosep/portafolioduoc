5101cf74203a44dfe673de019d8bbebbff248ed3
iosep <onarson@gmail.com>
2016-09-06 01:29:24 -0300

    muero de sueño!

---
+++ java/radioTaxi/src/BLL/Taxi.java
@@ -0,0 +1,31 @@
+package BLL;
+
+/**
+ *
+ * @author JoseOnate
+ */
+public class Taxi {
+
+    private String patente;
+    private char estado;
+
+    public Taxi() {
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+
+}
