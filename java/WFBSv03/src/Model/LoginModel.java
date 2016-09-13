d4956eb0035a9ecc6b6b6139da944f1cd45ffad5
iosep <onarson@gmail.com>
2016-09-13 17:25:10 -0300

    update java v03

--- java/WFBSv03/src/Model/LoginModel.java
+++ java/WFBSv03/src/Model/LoginModel.java
@@ -55,4 +55,47 @@ public class LoginModel {
         return userId;
     }
 
+    /**
+     * Método para obtener el RUT del usuario en sesión, retorna 0 si no
+     * encuentra coincidencias.
+     *
+     * @param userId
+     * @return
+     */
+    public String getRut(int userId) {
+        String rut = "";
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:dotnet/dotnet@localhost");
+            stmt = con.prepareStatement("SELECT rut FROM usuario WHERE nombre = ?");
+            stmt.setInt(1, userId);
+            rs = stmt.executeQuery();
+            if (!rs.next()) {
+                System.out.println("Sin coincidencias");
+                stmt.close();
+                con.close();
+                return 0;
+            } else {
+                rs = stmt.executeQuery();
+                while (rs.next()) {
+                    rut = rs.getString(1);//rut
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return rut;
+    }
+
 }
